fcb9e37140a2e894b7deb0ee8a01139808fdca04
munaimun <munaimunz@gmail.com>
2024-05-27 14:10:18 +0600

    created my new portfolio, full responsive

---
+++ src/components/Banner.js
@@ -0,0 +1,64 @@
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/profile.jpg";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Banner = () => {
+  const renderLetters = (text) => {
+    return text.split("").map((char, index) => (
+      <span key={index} className="hover-letter">
+        {char}
+      </span>
+    ));
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>
+                    {`Hi! I'm Fahad,`}{" "}
+                    <span className="txt-static">
+                      {renderLetters("Frontend React Developer")}
+                    </span>
+                  </h1>
+                  <p>
+                    Final-year student at Sonargaon University, deeply
+                    passionate about React development. Actively pursuing
+                    internship/job opportunities to apply and enhance my skills.
+                  </p>
+                  <button>
+                    Let’s Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img src={headerImg} alt="Header Img" />
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ src/components/Projects.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import intelitalk from "../assets/img/intelitalk.png";
+import topcourses from "../assets/img/topcourses.jpg";
+import viewfinder from "../assets/img/viewfinder.jpg";
+import quizjs from "../assets/img/quiz.jpg";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+const projects = [
+  {
+    title: "InteliTalk- Chatbot",
+    description:
+      "Created a chatbot application that utilizes Axios to handle API requests for seamless data fetching from the backend and implements secure user authorization using JWT. A streamlined chat interface for accessing university information, complemented by an admin panel for efficient database and student list management.",
+    imgUrl: intelitalk,
+    liveLink: "https://intelitalk.onrender.com/",
+    githubLink: "https://github.com/dev-saiful/InteliTalk/tree/frontend",
+  },
+  {
+    title: "Top Courses - Course catalog application",
+    description:
+      "A dynamic course catalog application that interacts with an API to fetch and display various courses.The user interface includes some category buttons for easy course filtering. Implemented a toast feature, allowing users to 'like' and receive feedback on their preferred courses instantly.",
+    imgUrl: topcourses,
+    liveLink: "https://top-courses-eight-ecru.vercel.app/",
+    githubLink: "https://github.com/Munaimun/TopCourses",
+  },
+  {
+    title: "ViewFinder - Tour Packages Application",
+    description:
+      "Developed a comprehensive Tour Packages Application, featuring detailed information on various tourist destinations. Implemented a user-friendly interface to showcase package details, enhancing the user experience - React, Javascript, HTML, Tailwind, CSS.",
+    imgUrl: viewfinder,
+    liveLink: "https://view-finder-nine.vercel.app/",
+    githubLink: "https://github.com/Munaimun/ViewFinder",
+  },
+  {
+    title: "Quiz App",
+    description:
+      "Developed a quiz application using Vanilla JS that asks questions about JavaScript concepts, testing users' knowledge and providing an engaging learning experience.",
+    imgUrl: quizjs,
+    liveLink: "https://munaimun.github.io/Quiz_js/",
+    githubLink: "https://github.com/Munaimun/Quiz_js",
+  },
+];
+
+export const Projects = () => {
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <h2>Projects</h2>
+                  <div className="container project-page" id="container">
+                    {projects.map((project, index) => (
+                      <div className="card" key={index}>
+                        <div>
+                          <img
+                            className="project-image"
+                            src={project.imgUrl}
+                            alt="Project"
+                          />
+                        </div>
+                        <div className="project-info">
+                          <h2 className="title">{project.title}</h2>
+                          <p className="description">{project.description}</p>
+                          <div className="buttons">
+                            <a
+                              className="project-link"
+                              href={project.liveLink}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              View Project
+                            </a>
+                            <a
+                              className="project-link"
+                              href={project.githubLink}
+                              target="_blank"
+                              rel="noopener noreferrer"
+                            >
+                              Github Source Code
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      {/* <img className="background-image-right" src={colorSharp2}></img> */}
+    </section>
+  );
+};
